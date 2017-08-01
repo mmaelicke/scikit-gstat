5daded97bb330646147f26ee73157adce6835d16
helge <info.helgeschneider@gmail.com>
2017-08-01 10:33:23 +0200

    Added Data Files from geostat Without decorators Changed import paths

---
+++ skgstat/__init__.py
@@ -0,0 +1,7 @@
+from .binning import binify_even_width, group_to_bin
+from .Variogram import Variogram
+from .optimize import VariogramFitter
+from .estimator import matheron, cressie, dowd, genton, minmax
+
+# from .DirectionalVariogram import DirectionalVariogram
+# from .OrdinaryKriging import OrdinaryKriging
