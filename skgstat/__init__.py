ee39fafa2c8bb48d67506225f2418775609c2038
helge <info.helgeschneider@gmail.com>
2017-12-15 10:33:25 +0100

    Merge branch 'dev' of C:\Users\helge\Dropbox\Praktikum\Projekte\scikit-gstat with conflicts.

--- skgstat/__init__.py
+++ skgstat/__init__.py
@@ -1,4 +1,5 @@
 from .binning import binify_even_width, group_to_bin
+from .distance import nd_dist
 from .Variogram import Variogram
 from .optimize import VariogramFitter
 from .estimator import matheron, cressie, dowd, genton, minmax
