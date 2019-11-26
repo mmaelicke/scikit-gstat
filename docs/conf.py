30bc1b74da8208095d74a59bb5810d0c35cda91c
Mirko Mälicke <mirko@maelicke-online.de>
2019-11-26 13:16:33 +0100

    init v0.2.8 dev branch

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Mirko Mälicke'
 author = 'Mirko Mälicke'
 
 # The short X.Y version
-version = '0.2.7'
+version = '0.2.8'
 # The full version, including alpha/beta/rc tags
-release = '0.2.7'
+release = '0.2.8'
 
 
 # -- General configuration ---------------------------------------------------
