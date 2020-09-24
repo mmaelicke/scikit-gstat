Bugfix for angle calculation for compass shape

skgstat/DirectionalVariogram.py
@@ -757,7 +757,7 @@ class DirectionalVariogram(Variogram):
         xmax = np.max(local_ref[:,0])
 
         # calculate the radius and y coordinates
-        r = xmax / a
+        r = xmax / np.cos(a)
         y = r * np.sin(a)
 
         # build and return the figure