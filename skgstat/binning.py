eb19733dd0e644a8d5f0a294aa64f0b45a288802
Mirko Mälicke <mirko@hydrocode.de>
2021-02-25 10:05:09 +0100

    new binning function

--- skgstat/binning.py
+++ skgstat/binning.py
@@ -10,22 +10,24 @@ def even_width_lags(distances, n, maxlag):
     Parameters
     ----------
     distances : numpy.array
-        Flat numpy array representing the upper triangle of the distance matrix.
-    n: integer
+        Flat numpy array representing the upper triangle of
+        the distance matrix.
+    n : integer
         Amount of lag classes to find
     maxlag : integer, float
         Limit the last lag class to this separating distance.
 
     Returns
     -------
-    numpy.array
+    bin_edges : numpy.ndarray
+        The **upper** bin edges of the lag classes
 
     """
     # maxlags larger than the maximum separating distance will be ignored
     if maxlag is None or maxlag > np.nanmax(distances):
         maxlag = np.nanmax(distances)
 
-    return np.linspace(0, maxlag, n + 1)[1:]
+    return np.linspace(0, maxlag, n + 1)[1:], None
 
 
 def uniform_count_lags(distances, n, maxlag):
@@ -37,15 +39,17 @@ def uniform_count_lags(distances, n, maxlag):
     Parameters
     ----------
     distances : numpy.array
-        Flat numpy array representing the upper triangle of the distance matrix.
-    n: integer
+        Flat numpy array representing the upper triangle of
+        the distance matrix.
+    n : integer
         Amount of lag classes to find
     maxlag : integer, float
         Limit the last lag class to this separating distance.
 
     Returns
     -------
-    numpy.array
+    bin_edges : numpy.ndarray
+        The **upper** bin edges of the lag classes
 
     """
     # maxlags larger than the maximum separating distance will be ignored
@@ -58,4 +62,47 @@ def uniform_count_lags(distances, n, maxlag):
     return np.fromiter(
         (np.nanpercentile(d, (i / n) * 100) for i in range(1, n + 1)),
         dtype=float
-    )
+    ), None
+
+
+def auto_derived_lags(distances, method_name, maxlag):
+    """Derive bins automatically
+    .. vserionadded:: 0.3.8
+
+    Uses `histogram_bin_edges <numpy.histogram_bin_edges>` to derive the
+    lag classes automatically. Supports any method supported by
+    `histogram_bin_edges <numpy.histogram_bin_edges>`. It is recommended
+    to use `'stuges'`, `'doane'` or `'fd'`.
+
+    Parameters
+    ----------
+    distances : numpy.array
+        Flat numpy array representing the upper triangle of
+        the distance matrix.
+    maxlag : integer, float
+        Limit the last lag class to this separating distance.
+    method_name : str
+        Any method supported by
+        `histogram_bin_edges <numpy.histogram_bin_edges>`
+
+    Returns
+    -------
+    bin_edges : numpy.ndarray
+        The **upper** bin edges of the lag classes
+
+    See Also
+    --------
+    numpy.histogram_bin_edges
+
+    """
+    # maxlags largher than maximum separating distance will be ignored
+    if maxlag is None or maxlag > np.nanmax(distances):
+        maxlag = np.nanmax(distances)
+
+    # filter for distances < maxlag
+    d = distances[np.where(distances <= maxlag)]
+
+    # calculate the edges
+    edges = np.histogram_bin_edges(d, bins=method_name)[1:]
+
+    return edges, len(edges)
