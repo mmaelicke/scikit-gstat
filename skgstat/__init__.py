f7794872ed307e9854c45a63c9453e21cb3c3b2d
Mirko Mälicke <mirko@hydrocode.de>
2021-03-22 09:10:32 +0100

    0.4.0

--- skgstat/__init__.py
+++ skgstat/__init__.py
@@ -5,6 +5,6 @@ from .Kriging import OrdinaryKriging
 from . import interfaces
 
 # set some stuff
-__version__ = '0.3.11'
+__version__ = '0.4.0'
 __author__ = 'Mirko Maelicke <mirko.maelicke@kit.edu>'
 __backend__ = 'matplotlib'
