5977885fb0a8beb9ea066bee8c8f9deb467dbd48
Mirko Mälicke <mirko@hydrocode.de>
2021-05-18 10:29:47 +0200

    added citation info to data module

--- skgstat/data/__init__.py
+++ skgstat/data/__init__.py
@@ -4,11 +4,27 @@ from skgstat.data._loader import field_names
 # define all names
 names = field_names()
 
+origins = dict(
+    pancake="""Image of a pancake with apparent spatial structure.
+    If you use this data, cite SciKit-GStat: https://doi.org/10.5281/zenodo.1345584
+    Copyright Mirko Mälicke, 2020."""
+)
+
 
 # define the functions
 def pancake(N=500, band=0, seed=42):
-    return _loader.get_sample('pancake', N=N, seed=seed, band=band)
+    sample = _loader.get_sample('pancake', N=N, seed=seed, band=band)
+
+    return dict(
+        sample=sample,
+        origin=origins.get('pancake')
+    )
 
 
 def pancake_field(band=0):
-    return _loader.field('pancake', band)
+    sample = _loader.field('pancake', band)
+
+    return dict(
+        sample=sample,
+        origin=origins.get('pancake')
+    )
--- skgstat/tests/test_data_loader.py
+++ skgstat/tests/test_data_loader.py
@@ -8,22 +8,22 @@ def test_data_names():
 
 
 def test_loader():
-    img = data.pancake_field()
+    img = data.pancake_field().get('sample')
 
     assert img.shape[0] == 500 and img.shape[1] == 500
 
 
 def test_sample():
-    c, v = data.pancake(N=50)
+    c, v = data.pancake(N=50).get('sample')
 
     assert len(c) == len(v) == 50
 
 
 def test_loader_mean():
-    c0, v0 = data.pancake(N=10, band=0)
-    c1, v1 = data.pancake(N=10, band=1)
-    c2, v2 = data.pancake(N=10, band=2)
-    cm, cv = data.pancake(N=10, band='mean')
+    c0, v0 = data.pancake(N=10, band=0).get('sample')
+    c1, v1 = data.pancake(N=10, band=1).get('sample')
+    c2, v2 = data.pancake(N=10, band=2).get('sample')
+    cm, cv = data.pancake(N=10, band='mean').get('sample')
 
     # manually calculate the mean
     mean = np.mean(np.column_stack((v0, v1, v2)), axis=1)
