270f4725d76825895593fe054088e725810f1de4
Mirko Mälicke <mirko@hydrocode.de>
2021-07-05 09:21:40 +0200

    0.6.4

--- skgstat/__init__.py
+++ skgstat/__init__.py
@@ -8,6 +8,6 @@ from . import data
 from . import util
 
 # set some stuff
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 __author__ = 'Mirko Maelicke <mirko.maelicke@kit.edu>'
 __backend__ = 'matplotlib'
