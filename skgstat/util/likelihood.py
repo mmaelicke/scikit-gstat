8b44936cea44b96a5ddef6d898ab855b53aa8d65
Mirko Mälicke <mirko.maelicke@kit.edu>
2022-02-02 12:41:20 +0100

    added doc to likelihood function

--- skgstat/util/likelihood.py
+++ skgstat/util/likelihood.py
@@ -28,6 +28,18 @@ Original documentation:
 {doc}
 """
 
+LIKE_DOC = """
+Negative log-likelihood function following [601].
+This likelihood function is based on a ``{model_name}`` model.
+The sample of the variogram instance contained ``{n}`` points.
+
+References
+----------
+[601]   Lark, R. M. "Estimating variograms of soil properties by the 
+        method‐of‐moments and maximum likelihood." European Journal 
+        of Soil Science 51.4 (2000): 717-728.
+"""
+
 
 def _model_transformed(model_func, has_s: bool = False) -> Callable:
     """
@@ -75,6 +87,29 @@ def _build_A(transformed_func: Callable, params: List[float], dists: np.ndarray)
 
 def get_likelihood(variogram: Variogram) -> Callable:
     """
+    Returns the maximum likelihood function for the given variogram.
+    The function takes a list of model parameters and returns the
+    negative log-likelihood for this set of parameters.
+    The signature of the reutrned function has the interface as
+    needed by :func:`scipy.optimize.minimize`.
+    The paramters for the models are given as ``[r, c0, b]``, which
+    are the effective range, sill and nugget. For the
+    :func:`Matérn <skgstat.models.matern>` and
+    :func:`Stable <skgstat.models.stable>` models, the parameters include
+    an additional parameter ``s``, which is the smoothness parameter.
+
+    Example
+    -------
+
+    .. code-block:: python
+
+        from scipy.optimize import minimize
+        like = get_likelihood(variogram)
+        res = minimize(like)
+
+    It is, hwowever, highly recommended to provide an initial guess and
+    specify bounds for the parameter space.
+
     """
     # extract the current data
     values = variogram.values
@@ -108,7 +143,7 @@ def get_likelihood(variogram: Variogram) -> Callable:
         # get the log of determinant of A
         D = np.log(det(A))
         # np.log(0.0) is -inf, so we need to check for this
-        if D == -np.inf:
+        if D == -np.inf:  # pragma: no cover
             # print a warning and return np.inf to not use these parameters
             print("Warning: D = -inf, returning np.inf")
             return np.inf
@@ -118,6 +153,9 @@ def get_likelihood(variogram: Variogram) -> Callable:
 
         # this is actually a 1x1 matrix
         return loglike.flatten()[0]
+
+    # add the docstring to the likelihood function
+    likelihood.__doc__ = LIKE_DOC.format(model_name=variogram.model.__name__, n=len(values))
 
     # return the likelikhood function
     return likelihood
