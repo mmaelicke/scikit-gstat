3dba8da2ebd09e5d829760b2436bae76a58e916c
Mirko Mälicke <mirko@hydrocode.de>
2022-12-05 20:58:04 +0100

    cross-variogram plotting draft

--- skgstat/plotting/cross_variogram_matrix.py
+++ skgstat/plotting/cross_variogram_matrix.py
@@ -1,6 +1,7 @@
 from typing import List
-import matplotlib as plt
-from skgstat.Variogram import Variogram
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 try:
     from plotly.subplots import make_subplots
@@ -10,20 +11,73 @@ except ImportError:
 
 
 def matplotlib_cv_matrix(
-    variograms: List[List[Variogram]],
+    variograms: List[List['Variogram']],
     add_model: bool = True,
     sharex: bool = True,
     sharey: bool = True
 ):
     """"""
-    raise NotImplementedError
+    # get the dimension
+    n_rows, n_cols = np.asarray(variograms).shape
+
+    # create the figure and axes
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(n_rows * 5, n_cols * 5), sharex=sharex, sharey=sharey)
+
+    # plot each of the primary / cross variograms  
+    for i in range(n_rows):
+        for j in range(n_cols):
+            # add the empirical variogram
+            v = variograms[i][j]
+            axes[i][j].plot(v.bins, v.experimental, 'b.')
+
+            # add the model
+            if add_model:
+                x = np.linspace(v.bins[0], v.bins[-1], num=100)
+                y = v.transform(x)
+                axes[i][j].plot(x, y, 'g-')
+
+    # return the figure
+    return fig
 
 
 def plotly_cv_matrix(
-    variograms: List[List[Variogram]],
+    variograms: List[List['Variogram']],
     add_model: bool = True,
     sharex: bool = True,
     sharey: bool = True
 ):
     """"""
-    raise NotImplementedError
+    # get the dimension
+    n_rows, n_cols = np.asarray(variograms).shape
+
+    # build the figure
+    fig = make_subplots(n_rows, n_cols, shared_xaxes=sharex, shared_yaxes=sharey)
+
+    # plot each of the primary / cross variograms  
+    for i in range(n_rows):
+        for j in range(n_cols):
+            # add the empirical variogram
+            v = variograms[i][j]
+            fig.add_trace(go.Scatter(
+                x=v.bins,
+                y=v.experimental,
+                mode='markers',
+                marker=dict(color='blue'),
+                showlegend=False
+            ), row = i + 1, col = j + 1)
+
+            # add the model
+            if add_model:
+                x = np.linspace(v.bins[0], v.bins[-1], num=100)
+                y = v.transform(x)
+                fig.add_trace(go.Scatter(
+                    x=x,
+                    y=y,
+                    mode='lines',
+                    line=dict(color='green'),
+                    showlegend=False
+                ), row = i + 1, col = j + 1)
+
+    # return the figure
+    return fig
+
