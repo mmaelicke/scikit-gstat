88339fc954cc2de9bf9b3db9c09555f2d8c4296b
Mirko Mälicke <mirko@hydrocode.de>
2023-07-05 14:34:39 +0200

    also exclude python 3.8

--- skgstat/tests/test_gstatsim_interface.py
+++ skgstat/tests/test_gstatsim_interface.py
@@ -10,7 +10,7 @@ coords, vals = skg.data.pancake(N=60).get('sample')
 variogram = skg.Variogram(coords, vals, maxlag=0.6, n_lags=12)
 
 # Test cases
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python >= 3.8 or higher")
 def test_grid_initialization_resolution():
     # Initialize grid with resolution
     grid = Grid(variogram, resolution=0.1)
@@ -20,7 +20,7 @@ def test_grid_initialization_resolution():
     assert grid.cols == 4731
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python >= 3.8 or higher")
 def test_grid_initialization_infer_resolution():
     # Initialize grid without rows/cols
     # Set resolution
@@ -29,14 +29,14 @@ def test_grid_initialization_infer_resolution():
     assert np.abs(grid.resolution - 6.3) < 0.01
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python >= 3.8 or higher")
 def test_grid_initialization_no_resolution_rows_cols():
     # Initialize grid without resolution or rows/cols (expecting an error)
     with pytest.raises(AttributeError):
         grid = Grid(variogram, resolution=None, rows=None, cols=None)
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python >= 3.8 or higher")
 def test_grid_resolution_setting():
     # Set resolution
     grid = Grid(variogram, rows=100, cols=100)
@@ -49,7 +49,7 @@ def test_grid_resolution_setting():
     assert grid.cols == 96
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python >= 3.8 or higher")
 def test_grid_rows_setting():
     grid = Grid(variogram, resolution=5)
     
@@ -61,7 +61,7 @@ def test_grid_rows_setting():
     assert np.abs(grid.resolution - 9.32) < 0.01
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python >= 3.8 or higher")
 def test_grid_cols_setting():
     grid = Grid(variogram, resolution=5)
     
@@ -73,7 +73,7 @@ def test_grid_cols_setting():
     assert np.abs(grid.resolution - 4.73) < 0.01
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python >= 3.8 or higher")
 def test_grid_prediction_grid():
     # Test prediction grid generation
     grid = Grid(variogram, resolution=0.1)
@@ -83,7 +83,7 @@ def test_grid_prediction_grid():
     assert prediction_grid.shape == (grid.shape[0] * grid.shape[1], 2)
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python >= 3.8 or higher")
 def test_grid_call_operator():
     # Test calling the Grid instance
     grid = Grid(variogram, resolution=0.1)
@@ -93,14 +93,14 @@ def test_grid_call_operator():
     assert prediction_grid.shape == (grid.shape[0] * grid.shape[1], 2)
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python >= 3.8 or higher")
 def test_grid_str_representation():
     # Test string representation
     grid = Grid(variogram, resolution=0.1)
     assert str(grid) == f'<Grid with {grid.rows} rows and {grid.cols} cols at {grid.resolution} resolution>'
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python >= 3.8 or higher")
 def test_prediction_grid_resolution():
     grid = prediction_grid(variogram, resolution=1, as_numpy=False)
 
@@ -109,7 +109,7 @@ def test_prediction_grid_resolution():
     assert grid.cols == 474
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python >= 3.8 or higher")
 def test_prediction_grid_cols_rows():
     grid = prediction_grid(variogram, cols=50, rows=50, as_numpy=True)
 
