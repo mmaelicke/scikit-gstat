a6b907216cacbfe699c85e1dcd5470d8c0e85a3a
Mirko Mälicke <mirko@hydrocode.de>
2026-01-16 08:41:51 +0100

    Fix documentation issues: correct tautological sentence and intersphinx mapping for gstools

--- docs/conf.py
+++ docs/conf.py
@@ -238,12 +238,7 @@ todo_include_todos = True
 
 # -- Intersphinx mapping -----------------------------------------------------
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3.6', None),
-    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
-    'numpy':  ('https://docs.scipy.org/doc/numpy', None),
-    'scipy':  ('https://docs.scipy.org/doc/scipy/reference', None),
-    'gstools': ('https://geostat-framework.readthedocs.io/projects/gstools/en/latest/', None),
-    'sklearn': ('http://scikit-learn.org/stable', None),
+    'gstools': ('https://geostat-framework.readthedocs.io/projects/gstools/en/stable/api.html', None),
 }
 
 from plotly.io._sg_scraper import plotly_sg_scraper
